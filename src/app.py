c449a34aeb40d57985118b2d8c320a9c33b07d03
Ramerlinj <ramerlint2008@gmail.com>
2026-02-12 09:48:42 -0400

    a

--- src/app.py
+++ src/app.py
@@ -1,11 +1,20 @@
+import os
+import json
 from flask import Flask, render_template
-from livereload import Server
+# from livereload import Server
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('main/index.html')
+    
+    json_url = os.path.join(app.root_path, 'static', 'data', 'products.json')
+    
+    
+    data_del_json = {}
+    with open(json_url, 'r', encoding='utf-8') as f:
+        data_del_json = json.load(f)
+    return render_template('main/index.html', products=data_del_json)
 
 
 @app.route('/products')
@@ -13,8 +22,5 @@ def menu():
     return render_template('main/menu.html')
 
 if __name__ == '__main__':
-    server = Server(app.wsgi_app)
-    server.watch('src/templates/')
-    server.watch('src/static/')
-    server.serve(host='localhost', port=5000)
+    app.run(debug=True)
     
